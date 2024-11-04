74af7558a0f2dbee0ab72f38057eb0d54dc56386
brandon-lerit <148146640+brandon-lerit@users.noreply.github.com>
2024-11-04 20:20:42 +0000

    Added New Resume

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,7 +21,7 @@ function Navbar() {
             <div className = "links">
                 <a href = "/#projects" className = "link-text">Projects</a>
                 <Link to="/about" className = "link-text">About</Link>
-                <a href = "https://drive.google.com/file/d/1pJM_w0ibJRqy2bRzOB1a8GDF2HpTLidg/view?usp=sharing" target="_blank" className = "link-text">
+                <a href = "https://drive.google.com/file/d/19Aqu4ygBqqP4ye_8QRAIdiIPxb4OiznV/view?usp=drive_link" target="_blank" className = "link-text">
                     Resume
                 </a>
             </div>
